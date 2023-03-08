bd60a9c830bc16f752b463290728435c359b5233
boris bozic <boris.bozic96@gmail.com>
2023-03-08 11:01:53 +0100

    Added server code

---
+++ app/includes/server.h
@@ -0,0 +1,19 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+// typedef struct server
+// {
+//     /* data */
+// };
+
+
+
+
+#endif
